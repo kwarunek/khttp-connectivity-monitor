95d670c80d160918878001b53f871a0d09f92f3d
Dominik Mostowiec <Dominik.Mostowiec@dreamlab.pl>
2024-08-14 15:39:29 +0200

    add response blob

--- pkg/receiver/receiver.go
+++ pkg/receiver/receiver.go
@@ -3,15 +3,18 @@ package receiver
 import (
 	"fmt"
 	"net/http"
-	"github.com/tarfik/khttp-connectivity-monitor/pkg/utils"
 	"strings"
+
+	"github.com/tarfik/khttp-connectivity-monitor/pkg/utils"
 )
 
 func SetupReceiver(addr string, region string, zone string, clusterName string, node string, reponse_size int64) {
 
 	json := fmt.Sprintf(`{"clusterName": "%s", "node": "%s", "zone": "%s", "region": "%s", "response": "RESPONSE"}`, clusterName, node, zone, region)
-    rand_bytes_cnt := reponse_size - int64(len(json)) + int64(len("RESPONSE"))
-	strings.Replace(json, "RESPONSE", string(utils.RandStringBytes(rand_bytes_cnt)), -1)
+	rand_bytes_cnt := reponse_size - int64(len(json)) + int64(len("RESPONSE"))
+	if rand_bytes_cnt > 0 {
+		json = strings.Replace(json, "RESPONSE", string(utils.RandStringBytes(rand_bytes_cnt)), -1)
+	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, json)
